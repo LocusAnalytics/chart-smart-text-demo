3551cb7427b6b70d5c21ca583f41c2ccc610dae3
Phillip Nguyen <nh273@cornell.edu>
2020-04-20 15:33:09 +0200

    adding collapse toggle

--- src/components/BarchartForm.js
+++ src/components/BarchartForm.js
@@ -1,30 +1,54 @@
 import React from "react";
+import Collapse from "@material-ui/core/Collapse";
+import Switch from "@material-ui/core/Switch";
 import StateSlider from "./Slider.js";
 import "../App.css";
 
 class BarchartForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: true,
+    };
+    this.toggleOpen = this.toggleOpen.bind(this);
+  }
+
+  toggleOpen = () => {
+    let oldState = this.state.open;
+    this.setState({ open: !oldState });
+  };
+
   handleChange = (sliderIndex, newSliderValue) => {
     this.props.updateData(sliderIndex, newSliderValue);
   };
 
   render() {
     return (
-      <form className="form barchart-form">
-        {this.props.data.map((datum) => {
-          return (
-            <div className="form-item-name" key={datum.index}>
-              {datum.name}:{" "}
-              <StateSlider
-                key={datum.index}
-                index={datum.index}
-                value={datum.val}
-                handleChange={this.handleChange}
-              />
-              <br></br>
-            </div>
-          );
-        })}
-      </form>
+      <div className="barchart-form">
+        <div className="barchart-collapse-control">
+          {" "}
+          Show controls{" "}
+          <Switch checked={this.state.open} onChange={this.toggleOpen} />
+        </div>
+        <Collapse in={this.state.open} collapsedHeight="5px">
+          <form className="form barchart-form">
+            {this.props.data.map((datum) => {
+              return (
+                <div className="form-item-name" key={datum.index}>
+                  {datum.name}:{" "}
+                  <StateSlider
+                    key={datum.index}
+                    index={datum.index}
+                    value={datum.val}
+                    handleChange={this.handleChange}
+                  />
+                  <br></br>
+                </div>
+              );
+            })}
+          </form>
+        </Collapse>
+      </div>
     );
   }
 }
